755f94f3d2f51003a0168da5a5aef3d97d263570
Soumya Mukherjee <soumyamukherjee@mail.com>
2021-09-09 22:20:50 +0530

    update #2a

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
-#!bin/bash
 
-sudo apt-get update && sudo apt install vlc telegram-desktop
+#!/bin/bash
+#need to add condition to check if distro is ubuntu
+sudo apt-get update && sudo apt install snapd gnome-tweak-tool vlc telegram-desktop
+sudo snap install chromium 
+sudo snap install code --classic
+

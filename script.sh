79126a8690c7e1df6a99bffc0308bec74388af04
Soumya Mukherjee <aymuosmukherjee@outlook.com>
2024-06-30 14:25:00 +0530

    Update script.sh with newer installs

--- script.sh
+++ script.sh
@@ -1,7 +1,46 @@
 
 #!/bin/bash
 #need to add condition to check if distro is ubuntu
+
+#install pyenv
+curl https://pyenv.run | bash
+# update env 
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+
+sudo apt-get install python-tk python3-tk tk-dev
+
+#install buildtools to build python from source
+sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc
+
 sudo apt-get update && sudo apt install snapd gnome-tweak-tool vlc telegram-desktop
 sudo snap install chromium 
 sudo snap install code --classic
+
+#brave installation
+sudo apt install curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
+
+# install nvm
+
+
+
+#restart shell 
+exec "$SHELL"
 
